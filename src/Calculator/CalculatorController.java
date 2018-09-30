53eb0caa733860c71e05510e5873c3c04e797e89
2ndTorpSquad <35653748+2ndTorpSquad@users.noreply.github.com>
2018-09-30 15:46:48 -0400

    Added CalculatorController Added CalculatorView Added Calculator Model

---
+++ src/Calculator/CalculatorController.java
@@ -0,0 +1,38 @@
+package Calculator;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class CalculatorController
+{
+	@FXML public Button buttonNum0;
+	@FXML public Button buttonNum1;
+	@FXML public Button buttonNum2;
+	@FXML public Button buttonNum3;
+	@FXML public Button buttonNum4;
+	@FXML public Button buttonNum5;
+	@FXML public Button buttonNum6;
+	@FXML public Button buttonNum7;
+	@FXML public Button buttonNum8;
+	@FXML public Button buttonNum9;
+	
+	@FXML public Button buttonOpeAdd;
+	@FXML public Button buttonOpeSub;
+	@FXML public Button buttonOpeMul;
+	@FXML public Button buttonOpeQuo;
+	@FXML public Button buttonOpeDec;
+	
+	@FXML public Button buttonClean;
+	
+	@FXML public Button TextField textFSol;
+	@FXML public Button Textfield textFEqu;
+	//when number button is clicked
+	
+	//when operator button is clicked
+	
+	//when equal button is clicked
+	
+	//when CE button is clicked
+	
+}
